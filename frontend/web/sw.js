06c81dfca10b36baf051ac81c48b3bc6cd3ce1b6
mwanginjamb <francnjamb@gmail.com>
2021-11-19 16:46:52 +0300

    Debug installation od service worker

--- frontend/web/sw.js
+++ frontend/web/sw.js
@@ -1,7 +1,27 @@
 // Install service worker
 
-self.addEventListener('install', e => {
+/*self.addEventListener('install', e => {
     console.log(`Service worker installed..`);
+});*/
+
+var CACHE_NAME = 'ess-v0.01';
+var urlsToCache = [
+  '/site',
+];
+
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  self.skipWaiting()
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache,{
+        redirect: 'follow'
+      });
+    })
+  );
 });
 
 // Listen to activate event
